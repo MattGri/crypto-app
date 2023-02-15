c5ca6f81b192509f9209b13a46a5524fc8ecaace
Mateusz <mateusz.grzybowski98@gmail.com>
2023-02-15 12:26:43 +0100

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
+    document.title = 'Crypto App';
+
     axios
       .get(
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
